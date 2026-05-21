6747419cdae009df44b6901c5307d79b288cb932
Sam Burdick <samuelmburdick@gmail.com>
2026-05-21 17:05:40 -0400

    Cube class skeletons

--- src/cube.rs
+++ src/cube.rs
@@ -1,7 +1,26 @@
 use rust_3d::Point3D;
 
+enum Basis {
+  X,
+  Z,
+}
+
+pub struct ZXCube {
+  x: Basis,
+  y: Basis,
+  z: Basis,
+}
+
+pub struct Port {
+
+}
+
+pub struct YHalfCube {
+
+}
+
 enum CubeKind {
-  
+  ZXCube, Port, YHalfCube
 }
 
 pub struct Cube {
