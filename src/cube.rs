4a0ffc2a3713dd3d8003114c1392ab188fe4bfb6
Sam Burdick <samuelmburdick@gmail.com>
2026-06-07 14:53:08 -0400

    cube class

--- src/cube.rs
+++ src/cube.rs
@@ -1,6 +1,6 @@
 use rust_3d::Point3D;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Basis {
   X,
   Z,
@@ -16,6 +16,9 @@ impl ZXCube {
   pub fn as_tuple(&self) -> (Basis, Basis, Basis) {
     return (self.x, self.y, self.z);
   }
+  pub fn is_spatial(&self) -> bool {
+    return self.x == self.y;
+  }
 }
 
 pub struct Port {
@@ -26,12 +29,26 @@ pub struct YHalfCube {
 
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum CubeKind {
   ZXCube, Port, YHalfCube
 }
+
 
 pub struct Cube {
   position: Point3D,
   kind: CubeKind,
   label: String
+}
+
+impl Cube {
+  pub fn is_zx_cube(&self) -> bool {
+    matches!(self.kind, CubeKind::ZXCube)
+  }
+  pub fn is_port(&self) -> bool {
+    matches!(self.kind, CubeKind::Port)
+  }
+  pub fn is_y_half_cube(&self) -> bool {
+    matches!(self.kind, CubeKind::YHalfCube)
+  }
 }
